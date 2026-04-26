9576fbeaf73f5edf8e3cb0c15107c20ce53c483e
Nirvika Rajendra <nirvikarajendra16@gmail.com>
2026-04-25 20:44:10 -0400

    fix: update API URL to production backend

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000/api'
+  // baseURL: 'http://localhost:8000/api'
+  baseURL: 'https://flowboard-backend-6aty.onrender.com/api'
 })
 
 api.interceptors.request.use((config) => {
